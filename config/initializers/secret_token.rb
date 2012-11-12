a6dd3ea7cb6266de265f303dffcb00df97bc7193
Cody Duval <cody.duval@gmail.com>
2012-11-12 11:05:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheBuzziest::Application.config.secret_token = 'ddb57d1cf517ed001f4bbe4db5f78a3450171160b806529c10a9337d06437ac63c0e1466f8596bb43323ff8de0b3fa77c4b6293eb29120eaee9cf850fde78ad2'
