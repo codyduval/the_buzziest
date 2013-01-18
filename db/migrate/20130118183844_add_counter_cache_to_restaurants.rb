4540a0bfa9317380638324adb1eb2b4261f2d33f
Cody Duval <cody.duval@gmail.com>
2013-01-18 14:28:01 -0500

    fixed migration

--- db/migrate/20130118183844_add_counter_cache_to_restaurants.rb
+++ db/migrate/20130118183844_add_counter_cache_to_restaurants.rb
@@ -1,10 +1,7 @@
 class AddCounterCacheToRestaurants < ActiveRecord::Migration
   def self.up
     add_column :restaurants, :buzz_mentions_count, :integer, :default => 0
-    Restaurant.find_each do |restaurant|
-      restaurant.update_attribute(:buzz_mentions_count, restaurant.buzz_mentions.length)
-      restaurant.save
-    end
+
   end
 
   def self.down
