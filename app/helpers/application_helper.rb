6d9381e178f4aa8be8e186285f0e62dac7f9a3cd
Cody Duval <cody.duval@gmail.com>
2013-01-18 10:49:37 -0500

    add page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def title(page_title)
+    if Rails.env.development?
+      page_title = " LOCAL" + page_title
+    end
+    content_for (:title) { " - " + page_title }
+  end
+
+  def yield_or_default(section, default = "")
+    if Rails.env.development?
+      default = " LOCAL"
+    end
+      content_for?(section) ? content_for(section) : default
+  end
 end
