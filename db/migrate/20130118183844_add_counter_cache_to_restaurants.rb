31b5cb3cbd2d8d0dacdbb6efb29994cb1b0147f3
Cody Duval <cody.duval@gmail.com>
2013-01-18 14:51:07 -0500

    updated migration with new counter cache

--- db/migrate/20130118183844_add_counter_cache_to_restaurants.rb
+++ db/migrate/20130118183844_add_counter_cache_to_restaurants.rb
@@ -2,6 +2,10 @@ class AddCounterCacheToRestaurants < ActiveRecord::Migration
   def self.up
     add_column :restaurants, :buzz_mentions_count, :integer, :default => 0
 
+    Restaurant.reset_column_information
+    Restaurant.find_each do |r|
+    Restaurant.reset_counters r.id, :buzz_mentions
+  end
   end
 
   def self.down
