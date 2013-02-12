63916a7f2a23caba6f4caf8ff701a505dfe13231
Cody Duval <cody.duval@gmail.com>
2013-02-12 13:07:10 -0500

    updated sentry_dsn var

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -3,5 +3,5 @@ require 'raven'
 SENTRY_DSN = 'https://27c7b1d1bf774ccabf7bf2da0d7154a9:07a150981b464fb6813237ff7eb60255@app.getsentry.com/5433'
 
 Raven.configure do |config|
-  config.dsn = SENTRY_DSN
+  config.dsn = 'SENTRY_DSN'
 end
