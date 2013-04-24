6be0e06a1d19ecc3f41220a63311e25c603babbe
Cody Duval <cody.duval@gmail.com>
2013-04-24 16:24:52 -0400

    fixed nil bug

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,7 +1,7 @@
 module RestaurantsHelper
   def latest_buzz(restaurant)
     buzz = BuzzScore.where(:restaurant_id => restaurant.id).last(:order => "id asc", :limit => 1)
-    buzz.buzz_score
+    latest_buzz_score = buzz.buzz_score ||= 0
   end
 
 end
