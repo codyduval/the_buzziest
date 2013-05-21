467034666e39dc9224faa5dcf51ada006f0cf56d
Cody Duval <cody.duval@gmail.com>
2013-05-21 09:24:14 -0400

    added restaurant client

--- lib/fetcher/restaurant/client.rb
+++ lib/fetcher/restaurant/client.rb
@@ -1,13 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
-require 'benchmark'
 
 module Fetcher
   module Restaurant
     class Client
+    attr_accessor :url  
       
-      def initialize(url)
-        @url = url
+      def initialize(url, node, page=1)
+        @url = url + "#{(page)}"
+        @page = page
+        @node = node
       end
 
       def fetch
@@ -16,13 +18,20 @@ module Fetcher
       end
 
       def parse(html)
+        restaurant_name_list = []
         doc = Nokogiri::HTML(html)
+        restaurant_names = doc.xpath(@node)
+        restaurant_names.each do |restaurant_name|
+          puts restaurant_name.text
+          restaurant_name_list.push(restaurant_name.text)
+        end
+        restaurant_name_list
       end
 
       private
 
       def get(url)
-        open(url, "User-Agent" => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_2; cs-cz) AppleWebKit/525.13 (KHTML, like Gecko) Version/3.1 Safari/525.13' ))
+        open(url, "User-Agent" => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_2; cs-cz) AppleWebKit/525.13 (KHTML, like Gecko) Version/3.1 Safari/525.13' )
       end
 
     end
--- test/fetcher/restaurant/client_test.rb
+++ test/fetcher/restaurant/client_test.rb
@@ -4,8 +4,11 @@ require "#{Rails.root}/lib/fetcher/restaurant/client.rb"
 module Fetcher
   module Restaurant
     describe Client do
-      context "fetch" do
-
+      it "#initialize" do
+        url = "http://www.testurl.com"
+        node = "node"
+        client = Client.new(url, node, 1)
+        client.url.must_equal url
       end
     end
   end
